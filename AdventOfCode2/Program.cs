23d307186caddf528acc268c4575d5819ad42fd3
Marco Littel <ml@newspark.nl>
2021-12-02 14:29:43 +0100

    Add project files.

---
+++ AdventOfCode2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var m = new MoveSub2();
+            m.MoveSub();
+            Console.WriteLine($"Down: {m.down} Forward: {m.forward} Product: {m.down*m.forward}  ");
+        }
+    }
+}
